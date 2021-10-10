66353b44d832bd4384cf1f12145630f6698e125b
Jordan Yee <jordanyee799@gmail.com>
2021-10-09 18:16:19 -0700

    commit tree cab6d699de11b768e05909e200dd263046df8124

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(x){
   // Write your code here!
+  if (x <= 400) {
+    return 'This one is on me!';
+  }
+  else if (x > 2000 && x <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (x > 2500) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(str){
   // Write your code here!
+  if (str === "NYC") {
+    return 'Ok, sounds good.';
+  } else {
+    return 'No go.';
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(str){
   // Write your code here!
+  switch(str){
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return "Bye.";
+  }
+  
+
 }
